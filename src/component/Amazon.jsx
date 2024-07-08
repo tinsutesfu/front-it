f0bf7aaeac60186084efc893fd4423b29fb17384
tinsutesfu <153826309+tinsutesfu@users.noreply.github.com>
2024-07-08 00:36:00 -0700

    Update Amazon.jsx

--- src/component/Amazon.jsx
+++ src/component/Amazon.jsx
@@ -105,7 +105,8 @@ const Amazon = ({ products }) => {
               <div className="product-image-container">
                 <img
                   className="product-image" 
-  
+  src={"https://front-it-solution.onrender.com/images/" + product.image}
+                  alt={product.image}
                 />
               </div>
 
