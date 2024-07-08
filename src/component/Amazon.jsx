5237da86c011774ff55f0622c0527e0869efae85
tinsutesfu <153826309+tinsutesfu@users.noreply.github.com>
2024-07-08 20:04:25 +0300

    Update Amazon.jsx

--- src/component/Amazon.jsx
+++ src/component/Amazon.jsx
@@ -105,9 +105,7 @@ const Amazon = ({ products }) => {
               <div className="product-image-container">
                 <img
                   className="product-image" 
- src={`${axios}/images/`+product.image}
-                  
-                />
+ src="images/lap.del.jpg"
               </div>
 
               <div className="product-name">
